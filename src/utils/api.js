081391c8dc35f8148aafa59a782c345bec0e31ba
klport <karlaportillo1@gmail.com>
2025-03-11 18:35:07 -0700

    project 11 trouble shooting

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
 const baseUrl = "http://localhost:3001";
 
 function getItems() {
-  return fetch(`$(baseUrl)/items`).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  return fetch(`${baseUrl}/items`).then((res) => { return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
   });
 }
 
